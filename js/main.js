026205cef5a2ee6b19a935b0bda615d25e88f852
404nofoundx <15304463351@163.com>
2021-11-30 19:09:21 +0800

    Site updated: 2021-11-30 19:09:20

--- js/main.js
+++ js/main.js
@@ -20,11 +20,7 @@
         $('.justified-gallery').justifiedGallery();
     }
 
-    if (typeof moment === 'function') {
-        $('.article-meta time').each(function() {
-            $(this).text(moment($(this).attr('datetime')).fromNow());
-        });
-    }
+
 
     $('.article > .content > table').each(function() {
         if ($(this).width() > $(this).parent().width()) {
@@ -134,4 +130,6 @@
         $mask.on('click', toggleToc);
         $('.navbar-main .catalogue').on('click', toggleToc);
     }
+$('div.container div.card[data-type=tags]').addClass('is-hidden-mobile');
+$('div.container div.card[data-type=archives]').addClass('is-hidden-mobile');
 }(jQuery, window.moment, window.ClipboardJS, window.IcarusThemeSettings));
